aeaf4c88e155baef876ab7ed8ba98c373461e76e
simon0820s <simon0820s@gmail.com>
2023-07-21 23:52:25 -0500

    abi

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -4,7 +4,7 @@ const FormSection = ({ type }) => {
   return (
     <div className="flex flex-col gap-1 w-60">
       <h2 className='font-bold text-lg'>{type}</h2>
-      <input className='px-2 py-1 rounded-sm text-gray-500' type="text" id={type} name={type} placeholder={type} required/>
+      <input className='px-2 py-1 bg-transparent text-gray-900' type="text" id={type} name={type} placeholder={type} required/>
     </div>
   )
 }
