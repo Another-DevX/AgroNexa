85a58350da1fe3954846e59f63842d817af1e636
simon0820s <simon0820s@gmail.com>
2023-07-21 23:31:24 -0500

    funtion form component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -12,30 +12,30 @@ export default function Form() {
     const data = Object.fromEntries(formData.entries())
     console.debug(data)
 
-    const response = await axios.post(
-      "https://api.pinata.cloud/pinning/pinJSONToIPFS",
-      JSON.stringify({
-        name: "testname",
-        description: "testdescription",
-      }),
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    console.debug(response.data);
+    // const response = await axios.post(
+    //   "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+    //   JSON.stringify({
+    //     name: "testname",
+    //     description: "testdescription",
+    //   }),
+    //   {
+    //     headers: {
+    //       Authorization: `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`,
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+    // );
+    // console.log(response.data);
   };
   return (
-    <form onSubmit={handleOnSubmit} className="flex flex-col items-center bg-slate-800 gap-8 py-8 px-4 rounded-lg w-80">
+    <form onSubmit={handleOnSubmit} className="flex flex-col items-center bg-slate-800 gap-8 py-8 px-4 rounded-lg">
       <div className="flex flex-col gap-2">
         <FormSection type="Name"/>
         <FormSection type="Description"/>
         <FormSection type="Image"/>
       </div>
 
-      <button className="px-4 py-2 bg-slate-500 font-bold rounded-lg ">Submit</button>
+      <button type="submit" className="px-4 py-2 bg-slate-500 font-bold rounded-lg ">Submit</button>
     </form>
   );
 };
