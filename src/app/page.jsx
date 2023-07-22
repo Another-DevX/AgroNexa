546c012287ff4523a89034404ed6aae22b4a64ce
simon0820s <simon0820s@gmail.com>
2023-07-21 20:57:47 -0500

    create form

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,13 +3,14 @@ import NftsGrid from "@/components/NftsGrid";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Image from "next/image";
 import json from '../components/template.json'
+import Form from "@/components/Form";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ConnectButton />
       <Nav/>
-      <NftsGrid nfts={json}/>
+      <Form/>
     </main>
   );
 }
--- src/components/NftsGrid.jsx
+++ src/components/NftsGrid.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 const NftsGrid = ({ nfts }) => {
   return (
-    <div className='grid grid-cols-2 grid-rows-2 gap-4'>
+    <div className='flex flex-col gap-10'>
           {
             nfts.map((nft) => {
               return (
-                <div key={nft.name} className=''>
-                  <img src={nft.img} className='w-10 h-20 rounded-sm'/>
+                <div key={nft.name} className='flex flex-col items-center'>
+                  <img src={nft.img} className='w-46 h-80 rounded-sm'/>
                   <h3 className='text-xs text-center'>{nft.name}</h3>
                 </div>
               )
