1316b59d729adf0cd11d396debcdc9ef0aad13da
simon0820s <simon0820s@gmail.com>
2023-07-21 20:09:22 -0500

    add imgs to grid

--- src/components/NftsGrid.jsx
+++ src/components/NftsGrid.jsx
@@ -1,10 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
-const NftsGrid = () => {
+const NftsGrid = ({ nfts }) => {
   return (
     <div className='grid grid-cols-2 grid-rows-2 gap-4'>
       <li className='bg-gray-600'>
-        
+        <ul>
+          {
+            nfts.map((nft) => {
+              return (
+                <div key={nft.name} className='rounded-sm'>
+                  <img height={100} width={200} src={nft.img}/>
+                </div>
+              )
+            })
+          }
+        </ul>
       </li>
     </div>
   )
