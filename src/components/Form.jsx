5ac6209c10a836ae4b48847778ba79416febbccb
simon0820s <simon0820s@gmail.com>
2023-07-21 23:01:07 -0500

    create basic form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,6 +9,7 @@ export default function Form() {
 
     const formData = new FormData(e.currentTarget)
     const data = Object.fromEntries(formData.entries())
+    console.log(data)
 
     const response = await axios.post(
       "https://api.pinata.cloud/pinning/pinJSONToIPFS",
@@ -26,36 +27,11 @@ export default function Form() {
     console.debug(response.data);
   };
   return (
-    <div className="flex justify-center">
-      <div className="w-60 h-1/2 p-6 flex flex-col items-start bg-[#0D1926] rounded-lg gap-1">
-        <div>
-          <h2 className="text-[#10E698] text-3xl xl:text-4xl font-bold">Create Your</h2>
-          <h2 className="text-[#10E698] text-3xl xl:text-4xl font-bold">Product</h2>
-        </div>
-        <p className="text-[#CFD1D4] text-xs xl:mt-2">mint your nft</p>
-
-        <div className="flex items-center justify-between gap-4">
-          <div className="flex-1 flex flex-col">
-            <span className="text-[#10E698]">Nombre</span>
-            <input
-              className="w-full text-gray-500 focus:outline-none px-1 rounded-sm"
-              placeholder="Nombre"
-            ></input>
-          </div>
-
-          <div className="flex-1 flex flex-col">
-            <span className="text-[#10E698]">Apellido</span>
-            <input
-              className="w-full text-gray-500 focus:outline-none px-1 rounded-sm"
-              placeholder="Apellido"
-            ></input>
-          </div>
-        </div>
-
-        <button onClick={handleOnSubmit} className="w-full py-1 xl:py-2 mt-4 rounded-md font-medium bg-[#10e698] hover:bg-[#10e69870] hover:text-neutral-100 transition-all ease-in-out duration-300">
-          Send
-        </button>
-      </div>
-    </div>
+    <form onSubmit={handleOnSubmit} className="flex flex-col items-center bg-slate-800">
+      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="Description" />
+      <input type="text" placeholder="Image" />
+      <button>Submit</button>
+    </form>
   );
 };
