d154e4e5a5d322266e1c8ec9d706c06720d8e7dc
simon0820s <simon0820s@gmail.com>
2023-07-21 19:05:55 -0500

    set components into page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,5 @@
+import Nav from "@/components/Nav";
+import NftsGrid from "@/components/NftsGrid";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Image from "next/image";
 
@@ -5,6 +7,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ConnectButton />
+      <Nav/>
+      <NftsGrid/>
     </main>
   );
 }
