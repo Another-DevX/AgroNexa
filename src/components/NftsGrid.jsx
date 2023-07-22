b2af0f81749ed9faa4493210c373407fbe34c8b6
simon0820s <simon0820s@gmail.com>
2023-07-21 20:19:30 -0500

    create grid

--- src/components/NftsGrid.jsx
+++ src/components/NftsGrid.jsx
@@ -4,19 +4,16 @@ import React from 'react'
 const NftsGrid = ({ nfts }) => {
   return (
     <div className='grid grid-cols-2 grid-rows-2 gap-4'>
-      <li className='bg-gray-600'>
-        <ul>
           {
             nfts.map((nft) => {
               return (
-                <div key={nft.name} className='rounded-sm'>
-                  <img height={100} width={200} src={nft.img}/>
+                <div key={nft.name} className=''>
+                  <img src={nft.img} className='w-10 h-20 rounded-sm'/>
+                  <h3 className='text-xs text-center'>{nft.name}</h3>
                 </div>
               )
             })
           }
-        </ul>
-      </li>
     </div>
   )
 }
